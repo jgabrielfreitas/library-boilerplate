44c8c191245970b7515d5d01bd66559da3af67ee
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-04-03 13:52:41 -0300

    foo list

--- library/src/main/java/com/jgabrielfreitas/boilerplate/Boilerplate.java
+++ library/src/main/java/com/jgabrielfreitas/boilerplate/Boilerplate.java
@@ -1,15 +1,26 @@
 package com.jgabrielfreitas.boilerplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by JGabrielFreitas on 03/04/17.
  */
 
 public class Boilerplate {
 
+  public List<Foo> getFooList() {
+    return fooList;
+  }
+
+  public void setFooList(List<Foo> fooList) {
+    this.fooList = fooList;
+  }
+
+  private List<Foo> fooList = new ArrayList<>();
+
   public static String returnHelloWorld() {
     return "Hello World - boilerplate library";
   }
-
-
 
 }
---
+++ library/src/main/java/com/jgabrielfreitas/boilerplate/Foo.java
@@ -0,0 +1,27 @@
+package com.jgabrielfreitas.boilerplate;
+
+/**
+ * Created by JGabrielFreitas on 03/04/17.
+ */
+
+public class Foo {
+
+  private String title;
+  private String subTitle;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getSubTitle() {
+    return subTitle;
+  }
+
+  public void setSubTitle(String subTitle) {
+    this.subTitle = subTitle;
+  }
+}
