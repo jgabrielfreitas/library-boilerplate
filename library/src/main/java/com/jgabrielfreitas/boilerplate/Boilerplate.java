b4d41dc23888f5177eadce4546e7aa79eb1267aa
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-04-03 12:26:10 -0300

    initial commit

---
+++ library/src/main/java/com/jgabrielfreitas/boilerplate/Boilerplate.java
@@ -0,0 +1,15 @@
+package com.jgabrielfreitas.boilerplate;
+
+/**
+ * Created by JGabrielFreitas on 03/04/17.
+ */
+
+public class Boilerplate {
+
+  public static String returnHelloWorld() {
+    return "Hello World - boilerplate library";
+  }
+
+
+
+}
